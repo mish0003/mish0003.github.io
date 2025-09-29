1e0d213f90724ad04a06d6715b0368e2c53017a1
mish0003 <mish0003@hz.nl>
2025-09-29 19:28:20 +0200

    Final touches

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,4 +1,3 @@
-
 let credits = 0;
 const creditElements = document.getElementsByClassName("credit");
 const gradeElements = document.getElementsByClassName("grade");
@@ -6,12 +5,12 @@ for (let i = 0; i < creditElements.length; i++) {
     if (gradeElements[i].innerText >= 5.5) {
         credits += parseFloat(creditElements[i].innerText);
     }
-
 }
-
 const progressDiv = document.getElementById("progress");
 const progress = ((credits * 100) / 60).toFixed(2);
 progressDiv.style.width = progress + "%";
+
+
 
 
 
